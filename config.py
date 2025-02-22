91e3b5cdce7ecf2855938c3df1a73a7db5a440a8
KRISHNA <154213033+krishnabhai99@users.noreply.github.com>
2025-02-22 15:40:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ class Config(object):
     # Turn this feature on or off using True or False put value inside  ""
     # TRUE for yes FALSE if no 
     TOKEN = True if os.environ.get('TOKEN', "True") == "True" else False 
-    SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "")
+    SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "anylinks.in")
     SHORTLINK_API = os.environ.get("SHORTLINK_API", "")
     VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 600)) # Add time in seconds
     IS_VERIFY = os.environ.get("IS_VERIFY", "True")
